7463b65cb2259917b7800b39b640109fb52337df
kjfatimagarcia <148573523+kjfatimagarcia@users.noreply.github.com>
2023-10-25 12:11:54 -0600

    Add files via upload

---
+++ music.js
@@ -0,0 +1,13 @@
+<script>
+			AFRAME.registerComponent('soundhandler', {
+				tick: function () {
+					   var entity = document.querySelector('[sound]');
+					 if (document.querySelector('a-marker').object3D.visible == true) {
+						entity.components.sound.playSound();
+					} else {
+						entity.components.sound.pauseSound();
+					}
+
+				 }
+			});
+		</script>
